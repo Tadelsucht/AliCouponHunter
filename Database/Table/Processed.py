a8c9ad1c59957f82d6083afff6f06035e88326f8
Julien B <tadelsucht@gmail.com>
2016-09-08 13:53:31 +0200

    BeautifulSoup

--- Database/Table/Processed.py
+++ Database/Table/Processed.py
@@ -1,16 +1,19 @@
-import sqlite3
 from datetime import datetime
+from Database.Database import Database
 
 
-class Processed:
-    def __init__(self):
-        super(self.__class__, self).__init__()
-
+class Processed(Database):
     _sql_table = u'''CREATE TABLE {0} (Shop TEXT, Keywords TEXT, URL TEXT, BestCouponDifference TEXT, AddedOrUpdated TIMESTAMP)'''
 
     def save(self, shop, keywords, url, bcd):
         self._cursor.execute(
-            u"INSERT INTO {0} (Shop TEXT, Keywords TEXT, URL TEXT, BestCouponDifference TEXT, AddedOrUpdated) VALUES (?,?,?,?,?);".format(
+            u"INSERT INTO {0} (Shop, Keywords, URL, BestCouponDifference, AddedOrUpdated) VALUES (?,?,?,?,?);".format(
                 self._database_name),
             (shop, keywords, url, bcd, datetime.now()))
         self._connection.commit()
+
+    def get_is_processed(self, url):
+        self._cursor.execute("SELECT COUNT(URL) FROM {0} WHERE URL = ?".format(self._database_name), [url])
+        if self._cursor.fetchone() > 0:
+            return True
+        return False
--- main.py
+++ main.py
@@ -1,26 +1,53 @@
-import urllib
+import logging
 
+import re
+import requests
+import time
 from BeautifulSoup import BeautifulSoup
 from py_bing_search import PyBingWebSearch
+from Database.Table.Processed import Processed
 
+# Config
+stop_error_number = 10
+sleep_time = 5
+headers = {'Accept-Encoding': 'deflate'}
 
+# DB
+db = Processed("ach.sqlite", "processed")
+
+# BING
 bing_api_key = '6gmJGqJOlN6VmeMkp0j4iA46Ayetcjz49YUfBh/7Nc4'
-search_term = "Python Software Foundation"
+search_term = 'site:www.aliexpress.com/store/ inbody:"Get coupon now"'
 
+# DO
 bing = PyBingWebSearch(bing_api_key, search_term, web_only=False)
 search_result = bing.search(format='json')
 for page in search_result:
-    # TODO: Check if URL is in Database, if not do your job, if the url is already there restart
+    url = page.url
+    if True:
+    #try:
+        if db.get_is_processed(url):
+            html = requests.get(url, headers=headers).text
+            soup = BeautifulSoup(html)
+            shop = soup.find("span", {"class": "shop-name"}).a.text
+            keywords = soup.find(attrs={"name": "keywords"})["content"]
+            print keywords
+            coupons = []
+            for coupon in soup.findAll("a", {"class": "get-coupon-btn"}):
+                discount = re.match('.*\$([0-9\.]+).*', str(coupon.find("span", {"class": "pay"}))).group(1)
+                minimum_purchase = re.match('.*\$([0-9\.]+).*', str(coupon.find("span", {"class": "get"}))).group(1)
+                coupons.append(float(minimum_purchase) - float(discount))
+            #if len(coupons) is not 0:
+                #db.save(shop, keywords, url, min(coupons))
+            #else:
+                #db.save(shop, keywords, url, None)
     '''
-    html_stream = urllib.urlopen(page.url)
-    soup = BeautifulSoup(html_stream)
-    keywords = soup.findAll(attrs={"name": "keywords"})
-    print keywords
+    except Exception, e:
+        logging.error("{0}: {1}".format(url, str(e)))
+        stop_error_number -= 1
+        if stop_error_number < 0:
+            logging.critical("To many errors!")
+            break
+        time.sleep(sleep_time)
     '''
-    print page.url
-
-#html_stream = urllib.urlopen("http://www.python.org/")
-#print html_stream.read()
-#soup = BeautifulSoup(html_stream)
-#keywords = soup.findAll(attrs={"name": "keywords"})
-#print keywords
+    break
