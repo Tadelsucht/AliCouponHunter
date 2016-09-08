b6076fd59bd88d01c61a3d18662ce3588804e3e1
Julien <@>
2016-09-08 16:52:53 +0200

    Numeric Coupon

--- Database/Table/Processed.py
+++ Database/Table/Processed.py
@@ -3,7 +3,7 @@ from Database.Database import Database
 
 
 class Processed(Database):
-    _sql_table = u'''CREATE TABLE {0} (ID INT PRIMARY KEY, Shop TEXT, Keywords TEXT, URL TEXT, BestCouponDifference TEXT, AddedOrUpdated TIMESTAMP)'''
+    _sql_table = u'''CREATE TABLE {0} (ID INT PRIMARY KEY, Shop TEXT, Keywords TEXT, URL TEXT, BestCouponDifference DOUBLE, AddedOrUpdated TIMESTAMP)'''
 
     def save(self, id, shop, keywords, url, bcd):
         self._cursor.execute(
