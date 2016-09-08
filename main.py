30bbd29db079da865b8b8f0a78ea2d5fd486de6e
Julien B <tadelsucht@gmail.com>
2016-09-08 14:45:53 +0200

    sleep time

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from Database.Table.Processed import Processed
 # Config
 maximum_bing_searches = 100
 stop_error_number = 10
-sleep_time = 10
+sleep_time = 16
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:48.0) Gecko/20100101 Firefox/48.0',
            'Accept-Encoding': 'deflate'}
 language_subdomain = "de"
