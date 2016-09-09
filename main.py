c9016509afb64e9ff7fd93a6f9e26c848b7980d8
Julien <tadelsucht@gmail.com>
2016-09-09 17:40:16 +0200

    Filter fix

--- main.py
+++ main.py
@@ -21,7 +21,9 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:48.0) Gecko/201
 language_subdomain = "de"
 logging.basicConfig(level=logging.INFO, format='%(asctime)s| %(message)s')
 logging.getLogger("requests").setLevel(logging.WARNING)
-cheapest_item_forbidden_words = [u'Zusätzlich', u'Gebühr', u'preis unterschied', u'Preisunterschied']
+forbidden_item_phrases = []
+with open('forbidden_item_phrases.txt', 'r', encoding='utf8') as f:
+    forbidden_item_phrases = f.readlines()
 
 # DB
 db = Processed("ach.sqlite", "processed")
@@ -107,7 +109,7 @@ while bing_search_counter is not maximum_bing_searches:
                     item_price = float(item[2].encode("ascii", "ignore"))
                     if item_price == 0.01:  # Default promo is 0.01, use other value for this case
                         item_price = float(item[4].encode("ascii", "ignore"))
-                    if (cheapest_item_price is None or item_price < cheapest_item_price) and all(word.lower() not in item_name for word in cheapest_item_forbidden_words):
+                    if (cheapest_item_price is None or item_price < cheapest_item_price) and all(word.lower() not in item_name for word in forbidden_item_phrases):
                         cheapest_item = item_name
                         cheapest_item_price = item_price
 
--- querys.sql
+++ querys.sql
@@ -5,12 +5,24 @@ WHERE Discount IS NOT NULL
 ORDER BY (CheapestItemPrice - Discount)  ASC
 
 
-SELECT * 
+DELETE
+--SELECT *
 FROM processed
 WHERE CheapestItem LIKE "%Zusätzlich%"
 	OR CheapestItem LIKE "%Gebühr%"
 	OR CheapestItem LIKE "%preis unterschied%"
 	OR CheapestItem LIKE "%Preisunterschied%"
+	OR CheapestItem LIKE "%Verschiffen nur%"
+	OR CheapestItem LIKE "%Porto%"
+	OR CheapestItem LIKE "%Bestellung%"
+	OR CheapestItem LIKE "%Zurückzahlen%"
+	OR CheapestItem LIKE "%geld zurück%"
+	OR CheapestItem LIKE "%Zahlung Zurück%"
+	OR CheapestItem LIKE "%konten%"
+	OR CheapestItem LIKE "%Anzahlung%"
+	OR CheapestItem LIKE "%Hinzufügen%"
+	OR CheapestItem LIKE "%differenz%"
+	OR CheapestItem LIKE "%transaktion%"
 	OR (CheapestItem LIKE "%Versandkosten%" AND CheapestItem NOT LIKE "%Versandkosten frei%")
 	
 
