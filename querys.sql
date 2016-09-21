9c3d21a5ed0bac917309059c1c84ca0648d76fc8
Julien <tadelsucht@gmail.com>
2016-09-21 11:31:07 +0200

    Configuration

--- querys.sql
+++ querys.sql
@@ -5,7 +5,7 @@ WHERE Discount IS NOT NULL
 ORDER BY Discount DESC, CheapestItemPrice ASC
 
 
-SELECT (CheapestItemPrice - Discount) AS 'Price', MinimumPurchase, CheapestItem, Keywords, Shop, URL, ID AS 'Price' 
+SELECT (CheapestItemPrice - Discount) AS 'Price', MinimumPurchase, CheapestItem, Keywords, Shop, URL, ID
 FROM processed 
 WHERE Discount IS NOT NULL 
 	AND  BestCouponDifference < 1
