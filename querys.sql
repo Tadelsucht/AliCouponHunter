d9ce9cecfafc55c1c250314a39fbbc4addd50f01
Julien B <stbtfl@t-online.de>
2016-11-21 10:29:47 +0100

    Update querys.sql

--- querys.sql
+++ querys.sql
@@ -3,6 +3,7 @@ FROM processed
 WHERE Discount IS NOT NULL 
 	AND BestCouponDifference < 1
 	AND CheapestItemPrice IS NOT NULL
+	--AND AddedOrUpdated > DATE(DATE('now'), '-7 day')
 ORDER BY Discount DESC, CheapestItemPrice ASC
 
 
@@ -11,7 +12,7 @@ FROM processed
 WHERE Discount IS NOT NULL 
 	AND  BestCouponDifference < 1
 	AND Price IS NOT NULL
-	AND AddedOrUpdated > DATE(DATE('now'), '-7 day')
+	--AND AddedOrUpdated > DATE(DATE('now'), '-7 day')
 ORDER BY (CheapestItemPrice - Discount)  ASC
 
 
